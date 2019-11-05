095d2657b2bc6316e3f02d283d891c647ce0ac09
michaelromanov <michael.alexander.romanov@gmail.com>
2019-11-04 21:36:53 -0500

    test commit

--- ig_clone.sql
+++ ig_clone.sql
@@ -1,1 +1,11 @@
-CREATE TABLE ();
+CREATE DATABASE ig_clone;
+USE ig_clone;
+
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+  username VARCHAR(255) UNIQUE NOT NULL, 
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+//source Instagram/ig_clone.sql
+//DESCRIBE users;
