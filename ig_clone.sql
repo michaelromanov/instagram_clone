2bae9d76d02ea1dad9d0b183b97e3b460d89ff7f
michaelromanov <michael.alexander.romanov@gmail.com>
2019-11-05 01:38:53 -0500

    building out database

--- ig_clone.sql
+++ ig_clone.sql
@@ -10,10 +10,35 @@ CREATE TABLE users (
 CREATE TABLE photos(
   id INT AUTO_INCREMENT PRIMARY KEY, 
   image_url VARCHAR(255) NOT NULL, 
-  user_id INTEGER NOT NULL, 
+  user_id INT NOT NULL, 
   created_at TIMESTAMP DEFAULT NOW(), 
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE comments(
+  comment_text VARCHAR(255), 
+  user_id INT NOT NULL,
+  photo_id INT NOT NULL
+);
+CREATE TABLE likes(
+  user_id INT NOT NULL,
+  photo_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(), 
+  FOREIGN KEY(user_id) REFERENCES users(id), 
+  FOREIGN KEY(photo_id) REFERENCES photos(id),
+  PRIMARY KEY(user_id, photo_id)
+);
+
 INSERT INTO users (username) VALUES ('BlueTheCat'), ('CharlieBrown'), ('ColtSteel');
 INSERT INTO photos (image_url, user_id) VALUES ('/alskjd76', 1), ('/lkajs98', 2), ('/90asds', 2);
+INSERT INTO comments (comment_text, user_id, photo_id) VALUES ('Meow', 1, 2), ('Amazing Shot!', 3, 2), ('I <3 This', 2, 1);
+INSERT INTO likes(user_id, )
+
+
+
+
+-- SELECT photos.image_url, users.username FROM photos JOIN users ON photos.user_id = users.id;
+
+
+
+
